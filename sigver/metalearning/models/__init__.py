6a3262e02697cdc37ec6c58052e029e075ac5886
Faisal Albasu <albasfaisal@yandex.ru>
2022-03-16 20:21:28 +0400

    removed metalearning, don't need it.

--- sigver/metalearning/models/__init__.py
+++
@@ -1,5 +0,0 @@
-import functools
-from . import signet, smallcnn
-
-available_models = {'signet': functools.partial(signet.SigNetModel, normalize=True),
-                    'smallcnn': smallcnn.SmallCNN}
