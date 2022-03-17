3c44770a52457bb706e59eb85fd65d07a3d1c043
Faisal Albasu <albasfaisal@yandex.ru>
2022-03-17 10:08:01 +0400

    Update __init__.py

--- sigver/datasets/__init__.py
+++ sigver/datasets/__init__.py
@@ -1,10 +1,3 @@
-from .gpds import GPDSDataset
-from .mcyt import MCYTDataset
-from .cedar import CedarDataset
-from .brazilian import BrazilianDataset, BrazilianDatasetWithoutSimpleForgeries
+from .sigcomp import SIGCOMPCHINESE
 
-available_datasets = {'gpds': GPDSDataset,
-                      'mcyt': MCYTDataset,
-                      'cedar': CedarDataset,
-                      'brazilian': BrazilianDataset,
-                      'brazilian-nosimple': BrazilianDatasetWithoutSimpleForgeries}
+available_datasets = {'sigcompchinese: SIGCOMPCHINESE'}
