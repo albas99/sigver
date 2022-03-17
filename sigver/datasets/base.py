d464d091289ef5021ad0e0eaff9e3b6d36dd9a4f
Faisal Albasu <albasfaisal@yandex.ru>
2022-03-17 09:29:59 +0400

    removed unneeded files and folders

--- sigver/datasets/base.py
+++ sigver/datasets/base.py
@@ -6,41 +6,41 @@ import numpy as np
 class IterableDataset(ABC):
     """ Base class for Datasets. The dataset classes specify information about
      the dataset (such as the number of signatures per user), as well as implement
-     methods to iterate over the signatures of an user. """
+     methods to iterate over the signatures of a user. """
 
-    @property
-    def maxsize(self):
-        """ Maximum size of the images in the dataset"""
-        raise NotImplementedError
-
-    @property
-    def genuine_per_user(self):
-        """ Number of genuine signatures per user"""
-        raise NotImplementedError
-
-    @property
-    def skilled_per_user(self):
-        """ Number of skilled forgeries per user"""
-        raise NotImplementedError
-
-    @property
-    def simple_per_user(self):
-        """ Number of simple forgeries per user"""
-        raise NotImplementedError
+    # @property
+    # def maxsize(self):
+    #     """ Maximum size of the images in the dataset"""
+    #     raise NotImplementedError
+    #
+    # @property
+    # def genuine_per_user(self):
+    #     """ Number of genuine signatures per user"""
+    #     raise NotImplementedError
+    #
+    # @property
+    # def skilled_per_user(self):
+    #     """ Number of skilled forgeries per user"""
+    #     raise NotImplementedError
+    #
+    # @property
+    # def simple_per_user(self):
+    #     """ Number of simple forgeries per user"""
+    #     raise NotImplementedError
+    #
+    # @abstractmethod
+    # def get_user_list(self) -> List[int]:
+    #     """ Returns the list of users in the dataset. """
+    #     pass
 
     @abstractmethod
-    def get_user_list(self) -> List[int]:
-        """ Returns the list of users in the dataset. """
-        pass
-
-    @abstractmethod
-    def iter_genuine(self, user: int) -> Iterable[Tuple[np.ndarray, str]]:
-        """ Iterates through the genuine signatures of an user
+    def iter_genuine(self, path: str) -> Iterable[Tuple[np.ndarray, str]]:
+        """ Iterates through the genuine signatures
 
         Parameters
         ----------
-        user : int
-            The user, from whom the signatures will be returned
+        path : str
+            The path where the signatures are stored
 
         Returns
         -------
@@ -49,30 +49,30 @@ class IterableDataset(ABC):
         """
         pass
 
+    # @abstractmethod
+    # def iter_simple_forgery(self, user: int) -> Iterable[Tuple[np.ndarray, str]]:
+    #     """ Iterates through the simple forgeries of an user
+    #
+    #     Parameters
+    #     ----------
+    #     user : int
+    #         The user, from whom the signatures will be returned
+    #
+    #     Returns
+    #     -------
+    #     Generator (iterable) of <image, filename>
+    #
+    #     """
+    #     pass
+
     @abstractmethod
-    def iter_simple_forgery(self, user: int) -> Iterable[Tuple[np.ndarray, str]]:
-        """ Iterates through the simple forgeries of an user
+    def iter_forgery(self, path: str) -> Iterable[Tuple[np.ndarray, str]]:
+        """ Iterates through the skilled forgeries
 
         Parameters
         ----------
-        user : int
-            The user, from whom the signatures will be returned
-
-        Returns
-        -------
-        Generator (iterable) of <image, filename>
-
-        """
-        pass
-
-    @abstractmethod
-    def iter_forgery(self, user: int) -> Iterable[Tuple[np.ndarray, str]]:
-        """ Iterates through the skilled forgeries of an user
-
-        Parameters
-        ----------
-        user : int
-            The user, from whom the signatures will be returned
+        path : str
+            The path where the signatures are stored
 
         Returns
         -------
