69c19c6a19a1a585c551e6a688cd08531a3bfa7f
Faisal Albasu <53338885+albas99@users.noreply.github.com>
2022-03-21 11:19:10 +0400

    Update sigcomp.py

--- sigver/datasets/sigcomp.py
+++ sigver/datasets/sigcomp.py
@@ -8,9 +8,10 @@ class SIGCOMPCHINESE(IterableDataset):
     """ Helper class to load SigComp Chinese Dataset
     """
 
-    def __init__(self, path='./Datasets/sigComp2011/trainingSet/Chinese'):
+    def __init__(self, path='./Datasets/sigComp2011/trainingSet/Chinese' extension='.png'):
         self.path = path
         self.users = list(range(1, 10+1))
+        self.extension = extension
 
     @property
     def genuine_per_user(self):
@@ -30,7 +31,7 @@ class SIGCOMPCHINESE(IterableDataset):
         """ Iterate through genuine signatures """
         #
         genuine_folder = os.path.join(self.path, 'Genuine')
-        files = ['{}_{}'.format(user, img) for img in range(1, 24)]
+        files = ['{}_{}{}'.format(user, img, extension) for img in range(1, 24)]
         for i in genuine_folder:
             full_path = os.path.join(genuine_folder, i)
             img = imread(full_path, as_gray=True)
@@ -39,7 +40,7 @@ class SIGCOMPCHINESE(IterableDataset):
     def iter_forgery(self, user):
         """ Iterate over forgeries """
         forge_folder = os.path.join(self.path, 'Forgeries')
-        files = ['{}_{}'.format(user, img) for img in range(1, 12)]
+        files = ['{}_{}{}'.format(user, img, extension) for img in range(1, 12)]
         for i in forge_folder:
             full_path = os.path.join(forge_folder, i)
             img = imread(full_path, as_gray=True)
